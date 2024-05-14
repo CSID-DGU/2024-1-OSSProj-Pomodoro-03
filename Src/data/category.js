8db9c1605a76b40157ca9d745c95e6b2261cdf17
KJW <kjw01024@naver.com>
2024-05-14 23:26:11 +0900

    디렉터리 구조 변경

--- Src/data/category.js
+++
@@ -1,8 +0,0 @@
-export const category = [
-    { img: "/category/total.svg", title: "AllPost", path: "/products" },
-    { img: "/category/food.svg", title: "FoodDelivery", path: "/vegetable" },
-    { img: "/category/shopping.svg", title: "DailyPurchase", path: "/fruit" },
-    { img: "/category/hobby.svg", title: "Hobby", path: "/grains" },
-   // { img: "/category/notepad.svg", title: "Posting", path: "/freepost" }
-    { img: "/category/anonymous.svg", title: "Annoymous", path: "/meat" },
-  ];
