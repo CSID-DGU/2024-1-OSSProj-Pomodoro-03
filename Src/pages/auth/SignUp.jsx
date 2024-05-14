64b83449f7ad3cc3b3b58c3ad8f347005c287781
JOU(김지우) <144210233+jouju3585@users.noreply.github.com>
2024-05-14 22:42:41 +0900

    Update SignUp.jsx

--- Src/pages/auth/SignUp.jsx
+++ Src/pages/auth/SignUp.jsx
@@ -86,7 +86,7 @@ function SignUp() {
   return (
     <>
       <Helmet>
-        <title>R09M - 회원가입</title>
+        <title>LIVE:ON - 회원가입</title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
