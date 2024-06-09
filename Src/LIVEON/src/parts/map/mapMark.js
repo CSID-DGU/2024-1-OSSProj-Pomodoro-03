86d058f3c81236881474fde60c61788999729214
nayoung601 <144122630+nayoung601@users.noreply.github.com>
2024-06-09 13:43:27 +0900

    feat : Add "mapMark.js" code

---
+++ Src/LIVEON/src/parts/map/mapMark.js
@@ -0,0 +1,46 @@
+import spot from '@/assets/icons/spot.svg';
+
+const { kakao } = window;
+
+let markers = [];
+
+export function mapMark(mapInstance, readRecordList, setSelectedRecord, selectedRecord) {
+  if (kakao && mapInstance) {
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    markers.forEach(marker => marker.setMap(null));
+    markers = [];
+
+    readRecordList?.forEach(record => {
+      const { meetingPoint } = record;
+      if (meetingPoint) {
+        geocoder.addressSearch(meetingPoint, (result, status) => {
+          if (status === kakao.maps.services.Status.OK) {
+            const { y, x } = result[0];
+            const coords = new kakao.maps.LatLng(y, x);
+            const imageSrc = spot;
+            const imageSize = new kakao.maps.Size(32, 32);
+            const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+            const marker = new kakao.maps.Marker({
+              map: mapInstance,
+              position: coords,
+              image: markerImage,
+            });
+
+            markers.push(marker);
+
+            kakao.maps.event.addListener(marker, 'click', () => {
+              if (selectedRecord && selectedRecord.id === record.id) {
+                setSelectedRecord(null);
+              } else {
+                setSelectedRecord(record);
+              }
+            });
+          }
+        });
+      }
+    });
+  } else {
+    console.error('kakao 또는 mapInstance에 접근할 수 없습니다.');
+  }
+}
