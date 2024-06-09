69bb06d99cb347bd805eb7ff65c72889addcaf33
nayoung601 <144122630+nayoung601@users.noreply.github.com>
2024-06-09 13:55:13 +0900

    chore : "category.js" code

--- Src/LIVEON/src/data/category.js
+++ Src/LIVEON/src/data/category.js
@@ -1,8 +1,8 @@
 export const category = [
-    { img: "/category/total.svg", title: "AllPost", path: "/products" },
-    { img: "/category/food.svg", title: "FoodDelivery", path: "/vegetable" },
-    { img: "/category/shopping.svg", title: "DailyPurchase", path: "/fruit" },
-    { img: "/category/hobby.svg", title: "Hobby", path: "/grains" },
-   // { img: "/category/notepad.svg", title: "Posting", path: "/freepost" }
-    { img: "/category/anonymous.svg", title: "Annoymous", path: "/meat" },
-  ];
+  { img: "/category/sharetotal.svg", title: "전체쉐어", path: "/products" },
+  { img: "/category/food.svg", title: "배달쉐어", path: "/food" },
+  { img: "/category/shopping.svg", title: "공동구매", path: "/shopping" },
+  { img: "/category/hobby.svg", title: "취미생활", path: "/hobby" },
+  { img: "/category/notepad.svg", title: "자유게시판", path: "/freeboard" },
+  { img: "/category/anonymous.svg", title: "익명게시판", path: "/anonymous" },
+];
